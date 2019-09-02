cbea72d7764df3ddfc625e5196cbbc1e737b3734
LifelessLoz <38470405+LifelessLoz@users.noreply.github.com>
2019-09-02 12:19:40 +0100

    Add files via upload
    javascript files

---
+++ js/carousel.js
@@ -0,0 +1,87 @@
+const track = document.querySelector('.carousel__track');
+const slides = Array.from(track.children);
+const workButton = document.querySelector('.work-button');
+const contactButton = document.querySelector('.contact-button');
+const homeButton = document.querySelector('.home-button');
+var slidePosition = 2;
+//console.log(slides);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+//console.log(slideWidth);
+
+const setSlidePosition = (slide, index) => {
+  slide.style.left = slideWidth * index + 'px';
+};
+slides.forEach(setSlidePosition);
+
+const orinalMove = (track) => {
+  track.style.transform = 'translateX(-' + slides[2].style.left + ')';
+}
+
+const moveToSlide = (track, targetSlide) => {
+  track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+}
+
+homeButton.addEventListener('mouseover', e => {
+  const nextSlide = slides[2];
+  slidePosition = 2;
+  moveToSlide(track, nextSlide);
+});
+
+document.getElementById("work-image").addEventListener('mouseover', e => {
+  const nextSlide = slides[4];
+  slidePosition = 4;
+  moveToSlide(track, nextSlide);
+});
+
+document.getElementById("contact-image").addEventListener('mouseover', e => {
+  const nextSlide = slides[0];
+  slidePosition = 0;
+  moveToSlide(track, nextSlide);
+});
+
+workButton.addEventListener('mouseover', e => {
+  if (slidePosition == 4){
+    null;
+  }
+  else {
+    const nextSlide = slides[3];
+    slidePosition = 3;
+    moveToSlide(track, nextSlide);
+  }
+});
+
+workButton.addEventListener('click', e => {
+  if (slidePosition == 4){
+    null;
+  }
+  else {
+    const nextSlide = slides[4];
+    slidePosition = 4;
+    moveToSlide(track, nextSlide);
+  }
+});
+
+contactButton.addEventListener('mouseover', e => {
+  if (slidePosition == 0){
+    null;
+  }
+  else {
+    const nextSlide = slides[1];
+    slidePosition = 1;
+    moveToSlide(track, nextSlide);
+  }
+});
+
+contactButton.addEventListener('click', e => {
+  if (slidePosition == 0){
+    null;
+  }
+  else {
+    const nextSlide = slides[0];
+    slidePosition = 0;
+    moveToSlide(track, nextSlide);
+  }
+});
+
+window.onload = orinalMove(track);
---
+++ js/work-expand.js
@@ -0,0 +1,24 @@
+$('.work-button').click(function() {
+    $('.carousel').css({
+        'height': '85vh',
+    });
+});
+
+$('#work-image').hover(function() {
+    $('.carousel').css({
+        'height': '85vh',
+    });
+});
+
+$('.home-button').hover(function() {
+  $('.carousel').css({
+      'height': '70vh',
+  });
+});
+
+
+$('.contact-button').hover(function() {
+  $('.carousel').css({
+      'height': '70vh',
+  });
+});
